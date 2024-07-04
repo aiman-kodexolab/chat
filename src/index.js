9f55a0c4157fdd3e41e854ca1cc4ee019bd5ebd6
aiman-kodexolab <aiman.zaheer@kodexolabs.com>
2024-07-04 12:25:37 +0500

    get key from script tag

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Chatbot from '../src/components/chatbot/Chatbot.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Chatbot from "../src/components/chatbot/Chatbot.jsx";
 
 // Function to render the chatbot in the given container
 window.renderChatbot = (containerId) => {
   const container = document.getElementById(containerId);
+  // onst scriptTag = document.getElementById('chatbot-script');
+  const key = container.getAttribute("data-key");
+  console.log("key", key);
   if (container) {
     const root = ReactDOM.createRoot(container);
     root.render(<Chatbot />);
@@ -16,7 +19,7 @@ window.renderChatbot = (containerId) => {
 };
 
 // Default rendering for the main app
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
