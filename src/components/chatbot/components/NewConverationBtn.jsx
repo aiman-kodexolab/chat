20a0d524bbac875f0db3a9b79cb9da511814c6bf
aiman-kodexolab <aiman.zaheer@kodexolabs.com>
2024-08-02 21:21:40 +0500

    Merge pull request #6 from aiman-kodexolab/fix/loaders
    fix: loaders

--- src/components/chatbot/components/NewConverationBtn.jsx
+++ src/components/chatbot/components/NewConverationBtn.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import { SendMsg } from '../../../assets';
+import React from "react";
+import { SendMsg } from "../../../assets";
 import "../style.css";
 
-const NewConversationBtn = ({ onClick, isLoading, buttonSize, className, style }) => (
-    <button
-      className={`${className}`}
-      onClick={onClick}
-      disabled={isLoading}
-      style={style}
-    >
-      {/* {isLoading ? ( */}
-        {/* // <Spinner height={buttonSize === 'large' ? "30" : "23"} color="#FFFFFF" /> */}
-      {/* ) : ( */}
+const NewConversationBtn = ({
+  onClick,
+  isFormLoading,
+  buttonSize,
+  className,
+  style,
+  isConversationListLoading,
+}) => (
+  <button
+    className={`${className} ${
+      (isFormLoading || isConversationListLoading) && className === "no_session"
+        ? "adjust_loader"
+        : ""
+    }`}
+    onClick={onClick}
+    disabled={isFormLoading || isConversationListLoading}
+    style={style}
+  >
+    <>
+      {isConversationListLoading || isFormLoading ? (
+        <div className="new-conversation-loader"></div>
+      ) : (
         <>
-          <img src={SendMsg} alt="" className="send-msg-icon" style={{width: '15px', height: '15px'}} />
-          <p className="new-conversation-text">
-            New Conversation
-          </p>
+          <img
+            src={SendMsg}
+            alt=""
+            className="send-msg-icon"
+            style={{ width: "15px", height: "15px" }}
+          />
+          <p className="new-conversation-text">New Conversation</p>
         </>
-      {/* )} */}
-    </button>
-  );
-  
-  export default NewConversationBtn;
+      )}
+    </>
+  </button>
+);
+
+export default NewConversationBtn;
