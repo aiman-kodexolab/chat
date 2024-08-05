5c0e7a4d5c49b5848e13abf7d55c804199ed73de
aiman-kodexolab <aiman.zaheer@kodexolabs.com>
2024-08-05 16:40:13 +0500

    feat: shadow dom

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import "./index.css";
 import ChatbotButton from "./components/chatBotButton/index.jsx";
 import App from "./App.js"
 import MyWidgetElement from "./class.js";
-
 // Function to render the chatbot in the given container
 window.renderChatbot = async (containerId) => {
   const container = document.getElementById(containerId);
-
   if (container) {
     const scriptTag = document.getElementById("chatbot-script");
     const apiKey = scriptTag.getAttribute("data-key");
-    const root = ReactDOM.createRoot(container);
+    const shadowRoot = container.attachShadow({ mode: 'open' });
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'widgetStyles.css';
+    shadowRoot.appendChild(linkElement);
+    const shadowContainer = document.createElement('div');
+    shadowRoot.appendChild(shadowContainer);
+    const root = ReactDOM.createRoot(shadowContainer);
     root.render(<ChatbotButton apiKey={apiKey} />);
+    // const root = ReactDOM.createRoot(container);
+    // root.render(<ChatbotButton apiKey={apiKey} />);
   } else {
     console.error(`Container with ID ${containerId} not found.`);
   }
 };
-
 // Default rendering for the main app
 // const rootElement = document.getElementById("root");
 // if (rootElement) {
