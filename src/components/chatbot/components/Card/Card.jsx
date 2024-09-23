23cbdc42be0a0c8bddf77f6b68c246b9770ae18c
aiman-kodexolab <aiman.zaheer@kodexolabs.com>
2024-09-23 13:12:27 +0500

    chore: remove console

--- src/components/chatbot/components/Card/Card.jsx
+++ src/components/chatbot/components/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css";
 
 const Card = ({ isToggled, children }) => {
-  console.log(children);
   return (
     <div className={`card-wrapper ${isToggled ? "light" : ""}`}>{children}</div>
   );
